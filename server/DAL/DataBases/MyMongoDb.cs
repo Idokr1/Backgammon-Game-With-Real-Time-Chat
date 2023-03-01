fa93112adcfa1b799a235d93fff458562b1ab156
Ido Krupik <idokrupik@gmail.com>
2023-03-01 20:54:27 +0200

    Update MyMongoDb.cs

--- server/DAL/DataBases/MyMongoDb.cs
+++ server/DAL/DataBases/MyMongoDb.cs
@@ -8,7 +8,7 @@ namespace DAL.DataBases
 
         public MyMongoDb(string database)
         {
-            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://idoBackgammon:FemCyBWvdjtXfArS@cluster0.svoxwoo.mongodb.net/?retryWrites=true&w=majority");
+            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://idoBackgammon:<password>@cluster0.svoxwoo.mongodb.net/?retryWrites=true&w=majority");
             var client = new MongoClient(settings);
             Client = client.GetDatabase(database);
         }
